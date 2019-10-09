19080385cd81ea2ca97f2c23360bf64047945080
sipho <siphomangena@outlook.com>
2019-10-09 15:41:35 +0200

    initial commit

---
+++ DevOps_Starter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+    "deb https://apt.dockerproject.org/repo/ \
+    ubuntu-$(lsb_release -cs) \
+    main" 
+sudo apt-get update
+sudo apt-get -y install docker-engine 
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+docker network create my-network 
+docker-compose up -d
---
+++ README.md
@@ -0,0 +1,7 @@
+# SlashTEC interview-test (Bayport) 
+ ############## To Start ####################
+ 1. Run this command: bash ./DevOps_Starter.sh
+ 2. Boom
+------------------If Any problems-------------
+1. check if you are using ubuntu 18.10+
+2. check if docker engine is installed and docker compose
